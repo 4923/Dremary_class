6a1ef2b8cccdee3e5d29b772c09884ed15bb963b
4923 <202003774y@gmail.com>
2020-08-11 19:14:32 +0900

    [ADD] make a django project and app

---
+++ page/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+  return render (request, 'home.html')
+
+# request가 들어오면 home 함수에서는 user가 보낸 요청(request)와 함께 home.html이라는 template를 return한다.
