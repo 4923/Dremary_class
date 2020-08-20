bfd2b46c86565833a6219ec3f518c3741ff85b1d
4923 <202003774y@gmail.com>
2020-08-21 00:30:25 +0900

    Lecture 6

--- page/urls.py
+++ page/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 # 이제부터 app에 있는 url은 page 앱의 urls.py에서 관리하니까 project에서는 각각의 앱을 연결만 하면 됨
-urlpatterns = [  # 아니 뭔데 대문자로 쓰니까 circular import error 뜨는데 Url -> url로 고치니까 해결됨
+urlpatterns = [  # 아니 뭔데 대문자로 쓰니까 circular import error 뜨고 서버 접근 거부됐는데 Url -> url로 고치니까 해결됨
   path('',views.home, name = "home"),
   path('introduce/', views.introduce, name = "introduce"),
   path('profile/<int:designer_id>',views.detail, name="detail"),
